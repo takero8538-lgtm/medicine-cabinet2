90e6d5eda6c7868394884b95098267eb114e4882
takero8538-lgtm <takero8538@gmail.com>
2025-11-15 09:29:15 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("v2").then(cache => {
+    caches.open("v3").then(cache => {
       return cache.addAll([
         "./",
         "./index.html",
@@ -17,3 +17,4 @@ self.addEventListener("fetch", event => {
   );
 
 });
+
